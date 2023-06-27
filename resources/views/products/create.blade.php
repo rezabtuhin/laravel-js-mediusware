9cf8933fd1c90c6d8e28be76075e288403a6cc12
rezabuddawla <rezabuddawlatuhin@gmail.com>
2023-06-28 02:02:55 +0600

    some modification in create product blade

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -5,6 +5,7 @@
         <h1 class="h3 mb-0 text-gray-800">Create Product</h1>
     </div>
     <form action="{{ route('product.store') }}" method="post" autocomplete="off" spellcheck="false">
+        @csrf
         <section>
             <div class="row">
                 <div class="col-md-6">
@@ -87,7 +88,7 @@
                     </div>
                 </div>
             </div>
-            <button type="button" class="btn btn-lg btn-primary">Save</button>
+            <button class="btn btn-lg btn-primary">Save</button>
             <button type="button" class="btn btn-secondary btn-lg">Cancel</button>
         </section>
     </form>
