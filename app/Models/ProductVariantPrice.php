0717305e2ddbe5998732e4cd0000075d96835d47
rezabuddawla <rezabuddawlatuhin@gmail.com>
2023-06-28 02:08:25 +0600

    defining fillable column for product variant and prices

--- app/Models/ProductVariantPrice.php
+++ app/Models/ProductVariantPrice.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProductVariantPrice extends Model
 {
-
+    protected $fillable = [
+        'product_variant_one',
+        'product_variant_two',
+        'product_variant_three',
+        'price',
+        'stock',
+        'product_id'
+    ];
 }
